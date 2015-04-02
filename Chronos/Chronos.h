34b42f39522ae71d1b9b85823c7172696e93a82e
schun <s.chun@utexas.edu>
2015-04-02 15:12:25 -0500

    Initial Commit

---
+++ Chronos/Chronos.h
@@ -0,0 +1,19 @@
+//
+//  Chronos.h
+//  Chronos
+//
+//  Created by Andrew Chun on 4/2/15.
+//  Copyright (c) 2015 com.zero223. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Chronos.
+FOUNDATION_EXPORT double ChronosVersionNumber;
+
+//! Project version string for Chronos.
+FOUNDATION_EXPORT const unsigned char ChronosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Chronos/PublicHeader.h>
+
+
